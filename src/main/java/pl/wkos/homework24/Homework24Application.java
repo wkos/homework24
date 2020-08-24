5b71dac62ae19d104cf5488f9aeed0108cffcdae
Wojciech Kos <obohp@wp.pl>
2020-08-24 16:59:24 +0200

    Initial commit

---
+++ src/main/java/pl/wkos/homework24/Homework24Application.java
@@ -0,0 +1,13 @@
+package pl.wkos.homework24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework24Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/wkos/homework24/Homework24ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wkos.homework24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
